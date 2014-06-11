51c646217bc11c8bb47c2c11ac26ab4b8d159199
kanosaki <kanosaki@gmail.com>
2014-06-11 14:24:33 +0900

    WebSocketのアドレスをJavaScriptで生成するようにした．
    WebSocketの接続時にトークンを採用するようにした方がいい？

--- quiz/static/quiz/js/lobby.js
+++ quiz/static/quiz/js/lobby.js
@@ -1,9 +1,13 @@
-
-(function(){
-    var ws_url = 'ws://localhost:8000/ws/lobby/' + window.quiz.lobby_id;
+(function () {
+    var server_host = window.location.host;
+    var ws_url = 'ws://' + server_host + '/ws/lobby/' + window.quiz.lobby_id;
 
     var socket = new WebSocket(ws_url);
-    socket.onmessage = function(sock_msg){
+    socket.send(JSON.stringify({
+        "type": "join",
+        "token": quiz.lobby_token
+    }));
+    socket.onmessage = function (sock_msg) {
         var msg = JSON.parse(sock_msg.data);
         if (msg.type == 'request_update') {
             // ...
