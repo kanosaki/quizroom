cd3364e09dd0d53dc5569e02257d6f3054701ea0
kanosaki <kanosaki@gmail.com>
2014-06-13 22:56:29 +0900

    Participantに関するテストを追加

--- quiz/test/test_viewmodels.py
+++ quiz/test/test_viewmodels.py
@@ -3,11 +3,26 @@ import json
 from django.test import TestCase, Client
 
 from quiz.control import active_lobby
+from quiz.models import Participant
 
 
 def decode_json(response):
     b = response.content
     return json.loads(b.decode('ascii'))
+
+
+class TestParticipant(TestCase):
+    fixtures = ['init_fixture.json']
+
+    def test_register(self):
+        c = Client()
+        self.assertEqual(Participant.objects.count(), 0)
+        url = '/user/register'
+        res = c.post(url, {'name': 'test_user'})
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(Participant.objects.count(), 1)
+        user = Participant.objects.get(pk=1)
+        self.assertEqual(user.name, 'test_user')
 
 
 class ActiveLobbyViewTest(TestCase):
@@ -66,3 +81,24 @@ class ControlLobbyTest(TestCase):
         self.assertEqual(lobby.active_quiz.body.caption, 'Second quiz')
 
 
+class TestViewLobby(TestCase):
+    fixtures = ['init_fixture.json']
+
+    def setUp(self):
+        control_url = '/lobby/now'
+        c = Client()
+        res = decode_json(c.post(control_url, {'id': 'default'}))
+        self.assertEqual(res['status'], 'ok')
+        lobby = active_lobby.get()
+        url = '/lobby/control/%d' % lobby.pk
+        c = Client()
+
+        # Check initial state
+        self.assertEqual(lobby.can_start, True)
+        self.assertEqual(lobby.active_quiz, None)
+
+        res = decode_json(c.post(url, {'command': 'activate'}))
+        self.assertEqual(res['status'], 'ok')
+
+    def test_normal_post(self):
+        pass
