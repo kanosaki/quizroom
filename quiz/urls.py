96114104a864170df60d09b87982d0fa136a6926
kanosaki <kanosaki@gmail.com>
2014-06-13 22:55:24 +0900

    使ってないURLを削除

--- quiz/urls.py
+++ quiz/urls.py
@@ -8,18 +8,8 @@ admin.autodiscover()
 
 participant_urls = patterns(
     '',
-    url(r'^Update$', viewmodels.UpdateParticipant.as_view(), name='participant_update'),
     url(r'^register$', viewmodels.CreateParticipant.as_view(), name='participant_register'),
-    url(r'^list$', viewmodels.UserList.as_view(), name='participant_list'),
-    url(r'^(?P<pk>\d+)/', viewmodels.ViewUser.as_view(), name='participant_view'),
     url(r'^signup$', viewmodels.CreateParticipant.as_view(), name='user_signup'),
-)
-
-quiz_urls = patterns(
-    '',
-    url(r'^list$', viewmodels.ListQuiz.as_view(), name='quiz_list'),
-    url(r'^(?P<pk>\d+)/$', viewmodels.ViewQuiz.as_view(), name='quiz_view'),
-    url(r'^(?P<pk>\d+)/submit_answer', views.submit_answer, name='quiz_submit_answer'),
 )
 
 lobby_urls = patterns(
@@ -31,7 +21,6 @@ lobby_urls = patterns(
 
 urlpatterns = patterns(
     '',
-    url(r'^quiz/', include(quiz_urls)),
     url(r'^user/', include(participant_urls)),
     url(r'^lobby/', include(lobby_urls)),
     url(r'^control/', views.game_control, name='game_control'),
