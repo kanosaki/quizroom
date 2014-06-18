768ff017679d4b75ead5b465a35c8512ea80224d
kihamu <dtbkihamu@yahoo.co.jp>
2014-06-18 12:45:05 +0900

    JSON.parseが動かないので削除

--- quiz/viewmodels.py
+++ quiz/viewmodels.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView  # , DeleteView
 from django.views.generic.detail import DetailView
